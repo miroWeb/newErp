e741b58409cc7d9e2b73742904d0eb07dbc6fdb9
miro web <mirodiljs2004@gmail.com>
2025-09-19 07:12:06 +0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-[red]">App</h1>
+      <p className="text-[blue]">This is a simple React application.</p>
+    </div>
+  );
+}
+
+export default App;
