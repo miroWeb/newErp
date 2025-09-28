6dd8dfb36aecb8f7511e1fca224659f0a8b82aaf
miro web <mirodiljs2004@gmail.com>
2025-09-28 20:47:26 +0500

    add list

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,15 @@ function Home() {
     <div>
         <h1 className="text-[red]">Home Page</h1>
         <p>Lorem ipsum dolor sit amet.</p>
+        <ul>
+          <li>home</li>
+          <li>home</li>
+          <li>home</li>
+          <li>home</li>
+          <li>home</li>
+          <li>home</li>
+          <li>home</li>
+        </ul>
     </div>
   )
 }
